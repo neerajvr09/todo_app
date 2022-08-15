e721d75370986bab4f57a112aff7c648868f1d59
Neeraj verma <74642854+neerajvr09@users.noreply.github.com>
2022-08-15 23:17:42 +0530

    added localStorage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import { useReducer } from "react";
+import { useEffect, useReducer } from "react";
 import "./App.css";
 import TodoForm from "./Components/TodoForm";
 import Todos from "./Components/Todos";
 import todoReducer from "./Context/reducer";
 import { TodoContext } from "./Context/TodoContext";
 
+const initializer = () => (
+  JSON.parse(localStorage.getItem('todos'))
+)
+
 const App = () => {
-  const [todos, dispatch] = useReducer(todoReducer, []);
+  const [todos, dispatch] = useReducer(todoReducer, [], initializer);
+
+  useEffect(()=>{
+    localStorage.setItem("todos", JSON.stringify(todos))
+  }, [todos])
 
   return (
     <TodoContext.Provider value={{ todos, dispatch }}>
